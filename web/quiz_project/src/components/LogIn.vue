d9874be5268ef46edcf52771c2293cce8cecde4a
jost243 <92725911+jost243@users.noreply.github.com>
2022-05-12 10:42:37 +0200

    Login vue

--- web/quiz_project/src/components/LogIn.vue
+++ web/quiz_project/src/components/LogIn.vue
@@ -2,13 +2,41 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
 export default {
-
     data() {
         return {
             Correo: "",
-            Contrasenya: ""
+            Contrasena: "",
+            Nombre: "",
+            Apellido: "",
+            estado: "",
         }
-    }
+    },
+    methods: {
+        login() {
+
+            const datosEnvio = new FormData();
+            datosEnvio.append("correo",this.Correo);
+            datosEnvio.append("contrasena",this.Contrasena);
+            datosEnvio.append("apellido",this.Apellido);
+            datosEnvio.append("nombre",this.Nombre)
+            fetch(`http://192.168.210.161:8000/login`, {
+                method: 'POST',
+                body: datosEnvio
+            }).then(res => {
+                return res.json();
+            }).then(data => {
+                console.log(data);
+                console.log(data.correo);
+                if(data.status == "Correo no registrado o incorrecto") {
+                    this.estado = "Correo no registrado o incorrecto";
+                } else if(data.status == "Contraseña incorrecta") {
+                    this.estado = "Contraseña Incorrecta";
+                } else if (data.correo == this.Correo && data.contrasena == this.Contrasena) {
+                    this.$router.push('/');
+                }
+            })
+        }
+    },
 }
 </script>
 
@@ -17,15 +45,15 @@ export default {
             <h1>Log In</h1>
             <div>
                  <label> Correo</label>
-                <input id="Correo" type="text" value="Correo" >
+                <input id="Correo" type="text" v-model="Correo"  >
                 <br>
-                 <label>Contrasenta</label>
-                <input id="Contrasenta" type="text" value="Contrasenya">
+                 <label>Contrasenya</label>
+                <input id="Contrasenta" type="password" v-model="Contrasena">
             </div>
-
-            <button id="login">Login</button>
+            <button id="login" @click="login()">Login</button>
             <RouterLink to="/register">
                 <button id="register" value="register">Register</button>
             </RouterLink>
+            <p>{{this.estado}}</p>
         </div>
 </template>
