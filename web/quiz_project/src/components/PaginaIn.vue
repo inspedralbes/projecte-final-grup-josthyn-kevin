ed5c80b86ca2c52eb118501b34d6c784bc653d45
jost243 <a20joslomdur@inspedralbes.cat>
2022-05-24 19:48:04 +0200

    Update PaginaIn.vue

--- web/quiz_project/src/components/PaginaIn.vue
+++ web/quiz_project/src/components/PaginaIn.vue
@@ -7,18 +7,34 @@ import { mapStores } from 'pinia'
       return {
       quizMasJugados: [],
       quizTemas: [],
-      quizMeUsuarios: []
+      quizMejores: []
       }
     },
     computed: {
         ...mapStores(sessioStore)
     },
    mounted () {
+
+       fetch(`http://192.168.1.148:8000/masjugados`) 
+      .then(res => res.json())
+      .then((data) => {
+        this.quizMasJugados = data;
+        console.log(this.quizMasJugados);
+      });
+
      fetch(`http://192.168.1.148:8000/tema`) 
       .then(res => res.json())
       .then((data) => {
         this.quizTemas = data;
         console.log(this.quizTemas)
+      });
+
+
+       fetch(`http://192.168.1.148:8000/mejores`) 
+      .then(res => res.json())
+      .then((data) => {
+        this.quizMejores = data;
+        console.log(this.quizMejores)
       });
     },
   }
@@ -36,17 +52,28 @@ import { mapStores } from 'pinia'
           <div v-for="(quizses,index) in this.quizMasJugados" :key="index">
                   <h5>{{quizses.Titulo}}</h5>
           </div>
+          <div v-for="(mejores,index) in this.quizMejores" :key="index">
+                  <p>{{mejores.nombre}}</p>
+                  <p>{{mejores.apellido}}</p>
+                  <p>{{mejores.puntuacion}}</p>
+          </div>
       </article> 
     </div>
+    
     <div v-else id="contenidor">
       <article>
-          <div v-for="(temas,index1) in this.quizTemas" :key="index1">
+          <div v-for="(temas,index) in this.quizTemas" :key="index">
               <RouterLink :to="`/usuario/${temas.id}`">{{temas.id}}{{temas.titulo}}</RouterLink>
           </div>
           <div v-for="(quizses,index) in this.quizMasJugados" :key="index">
                 <RouterLink :to="`/pregunta/${quizses.id}`">
                   <h5>{{quizses.Titulo}}</h5>
                 </RouterLink>
+          </div>
+          <div v-for="(mejores,index) in this.quizMejores" :key="index">
+                  <p>{{mejores.nombre}}</p>
+                  <p>{{mejores.apellido}}</p>
+                  <p>{{mejores.puntuacion}}</p>
           </div>
       </article>
       </div> 
