f3e0a98f940c0b607bb9d078166eec1d2a47df5a
jost243 <a20joslomdur@inspedralbes.cat>
2022-05-24 18:04:50 +0200

    vue

--- web/quiz_project/src/components/PaginaIn.vue
+++ web/quiz_project/src/components/PaginaIn.vue
@@ -6,17 +6,28 @@ import { mapStores } from 'pinia'
     data() {
       return {
       quizMasJugados: [],
+      quizTemas: [],
       }
     },
     computed: {
         ...mapStores(sessioStore)
     },
    mounted () {
+
+     //Fetch recupera los mejores quiz
       fetch(`http://192.168.1.148:8000/quiz/masJugados`) 
       .then(res => res.json())
       .then((data) => {
         this.quizMasJugados = data;
         console.log(this.quizMasJugados);
+      });
+
+      //Fetch recupera los temas
+      fetch(`http://192.168.1.148:8000/tema`) 
+      .then(res => res.json())
+      .then((data) => {
+        this.quizTemas = data;
+        console.log(this.quizTemas);
       });
     },
   }
@@ -27,6 +38,10 @@ import { mapStores } from 'pinia'
 
     <div v-if="!this.sessioStore.get.estadoLogin === true" id="contenidor">
       <article>
+
+          <div v-for="(temas,index1) in this.quizTemas" :key="index1">
+              <h5>{{temas.titulo}}</h5>
+          </div>
           <div v-for="(quizses,index) in this.quizMasJugados" :key="index">
                   <h5>{{quizses.Titulo}}</h5>
           </div>
@@ -35,11 +50,13 @@ import { mapStores } from 'pinia'
     </div>
     <div v-else id="contenidor">
       <article>
+          <div v-for="(temas,index1) in this.quizTemas" :key="index1">
+              <RouterLink :to="`/usuario/USUARIOQUEDARA`"><button @click="irQuiz()">Home</button></RouterLink>
+          </div>
           <div v-for="(quizses,index) in this.quizMasJugados" :key="index">
                 <RouterLink :to="`/pregunta/${quizses.id}`">
                   <h5>{{quizses.Titulo}}</h5>
                 </RouterLink>
-               
           </div>
            <RouterLink :to="`/usuario/${this.sessioStore.get.idUser}`"><button>Home</button></RouterLink>
       </article>
